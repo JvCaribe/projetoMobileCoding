b4eda1bb9fbe693a2e65aee10f9c8862691315a8
JvCaribe <joaovpcaribe@gmail.com>
2026-05-21 16:55:51 -0300

    Consertando cagada

--- AV2_filmes/src/app/busca/busca.page.ts
+++ AV2_filmes/src/app/busca/busca.page.ts
@@ -21,3 +21,55 @@ export class BuscaPage implements OnInit {
   }
 
 }
+
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItemDivider, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar, IonButton } from '@ionic/angular/standalone';
+import { FilmesService } from '../services/filmes';
+import { OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { IonIcon } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { IonSpinner } from '@ionic/angular/standalone';
+@Component({
+  selector: 'app-infos',
+  templateUrl: './infos.page.html',
+  styleUrls: ['./infos.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItemDivider, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonSearchbar, IonButton, IonIcon, RouterLink, IonSpinner],
+})
+export class InfosPage implements OnInit {
+
+  constructor(private filmesService: FilmesService) { }
+
+  filmes: any[] = [];
+  nomeFilme: string = '';
+
+  ngOnInit(): void {
+    this.buscarFilme();}
+
+  buscarFilme() {
+
+    this.filmesService.getFilmes(this.nomeFilme)
+      .subscribe({
+        next: (resposta: any) => {
+
+          if (resposta.Response === 'True') {
+            this.filmes = resposta.Search;
+          } else {
+            this.filmes = [];
+          }
+
+        },
+
+        error: (erro) => {
+          console.error('Erro ao buscar filmes', erro);
+          this.filmes = [];
+        }
+      });
+
+  }
+
+}
--- AV2_filmes/src/app/infos/infos.page.ts
+++ AV2_filmes/src/app/infos/infos.page.ts
@@ -1,51 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItemDivider, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar, IonButton } from '@ionic/angular/standalone';
-import { FilmesService } from '../services/filmes';
-import { OnInit } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItemDivider, IonLabel, IonIcon } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
-import { IonSpinner } from '@ionic/angular/standalone';
+
 @Component({
   selector: 'app-infos',
   templateUrl: './infos.page.html',
   styleUrls: ['./infos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItemDivider, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonSearchbar, IonButton, IonIcon, RouterLink, IonSpinner],
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItemDivider, IonLabel, RouterLink, IonIcon]
 })
 export class InfosPage implements OnInit {
 
-  constructor(private filmesService: FilmesService) { }
+  constructor() { }
 
-  filmes: any[] = [];
-  nomeFilme: string = '';
-
-  ngOnInit(): void {
-    this.buscarFilme();}
-
-  buscarFilme() {
-
-    this.filmesService.getFilmes(this.nomeFilme)
-      .subscribe({
-        next: (resposta: any) => {
-
-          if (resposta.Response === 'True') {
-            this.filmes = resposta.Search;
-          } else {
-            this.filmes = [];
-          }
-
-        },
-
-        error: (erro) => {
-          console.error('Erro ao buscar filmes', erro);
-          this.filmes = [];
-        }
-      });
-
+  ngOnInit() {
   }
 
 }
-
