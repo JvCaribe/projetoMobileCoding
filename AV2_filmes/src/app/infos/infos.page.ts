8864b2f181fe0c06963154a80f0a90b8bc5c4ed2
JvCaribe <joaovpcaribe@gmail.com>
2026-05-21 16:51:16 -0300

    Trazendo o TS pro Develop

--- AV2_filmes/src/app/infos/infos.page.ts
+++ AV2_filmes/src/app/infos/infos.page.ts
@@ -1,21 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItemDivider, IonLabel, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItemDivider, IonLabel, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar, IonButton } from '@ionic/angular/standalone';
+import { FilmesService } from '../services/filmes';
+import { OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { IonIcon } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
-
+import { IonSpinner } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-infos',
   templateUrl: './infos.page.html',
   styleUrls: ['./infos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItemDivider, IonLabel, RouterLink, IonIcon]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItemDivider, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonSearchbar, IonButton, IonIcon, RouterLink, IonSpinner],
 })
 export class InfosPage implements OnInit {
 
-  constructor() { }
+  constructor(private filmesService: FilmesService) { }
 
-  ngOnInit() {
+  filmes: any[] = [];
+  nomeFilme: string = '';
+
+  ngOnInit(): void {
+    this.buscarFilme();}
+
+  buscarFilme() {
+
+    this.filmesService.getFilmes(this.nomeFilme)
+      .subscribe({
+        next: (resposta: any) => {
+
+          if (resposta.Response === 'True') {
+            this.filmes = resposta.Search;
+          } else {
+            this.filmes = [];
+          }
+
+        },
+
+        error: (erro) => {
+          console.error('Erro ao buscar filmes', erro);
+          this.filmes = [];
+        }
+      });
+
   }
 
 }
+
