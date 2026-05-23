a243c533fc1f1caf6a361363b8f77d19b4d39571
JvCaribe <joaovpcaribe@gmail.com>
2026-05-23 16:31:47 -0300

    Ajeitando a entrada das Séries

--- AV2_filmes/src/app/infos/infos.page.ts
+++ AV2_filmes/src/app/infos/infos.page.ts
@@ -6,7 +6,7 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { FilmesService } from '../services/filmes.service';
 import { ClassificacaoPipe } from '../pipes/classificacao-pipe';
 import { CorNotaDiretiva } from './cor-nota';
-import {CorClassificacaoDiretiva } from './cor-classificacao';
+import { CorClassificacaoDiretiva } from './cor-classificacao';
 
 @Component({
   selector: 'app-infos',
@@ -39,19 +39,21 @@ export class InfosPage implements OnInit {
   duracao: string = '';
   linkImdb: string = '';
   plot: string = '';
+  temporadas: string = '';
+  dataReq: Date = new Date();
 
   ngOnInit() {
-  this.route.params.subscribe(params => {
+    this.route.params.subscribe(params => {
 
-    const id = params['id'];
+      const id = params['id'];
 
-    console.log('ID RECEBIDO:', id);
+      console.log('ID RECEBIDO:', id);
 
-    if (id) {
-      this.carregarFilme(id);
-    }
+      if (id) {
+        this.carregarFilme(id);
+      }
 
-  });
+    });
 
   }
 
@@ -70,18 +72,11 @@ export class InfosPage implements OnInit {
     this.tituloPagina = dados.Title;
     this.poster = dados.Poster;
     if (this.poster === 'N/A') {
-      this.poster = 'assets/icon/notfound.png';
+      this.poster = 'assets/icon/404nf.png';
     }
     this.title = dados.Title;
     this.genero = dados.Genre || 'N/A';
     this.anoLnc = dados.Year || 'N/A';
-    if (dados.Type === 'movie') {
-      this.tipo = 'Filme';
-    } else if (dados.Type === 'series') {
-      this.tipo = 'Série';
-    } else {
-      this.tipo = dados.Type || 'N/A';
-    }
     this.rottenRating = dados.Ratings?.[1]?.Value || 'N/A';
     this.imdbRating = dados.imdbRating || 'N/A';
     this.metascoreRating = dados.Metascore || 'N/A';
@@ -89,12 +84,32 @@ export class InfosPage implements OnInit {
     this.direcao = dados.Director || 'N/A';
     this.elenco = dados.Actors || 'N/A';
     this.premios = dados.Awards || 'N/A';
-    this.duracao = dados.Runtime || 'N/A';
     this.linkImdb = `https://www.imdb.com/title/${dados.imdbID}/`;
     this.plot = dados.Plot || 'N/A';
+
+    if (dados.Type === 'movie') {
+      this.tipo = 'Filme';
+    } else if (dados.Type === 'series') {
+      this.tipo = 'Série';
+    } else {
+      this.tipo = dados.Type || 'N/A';
+    }
+    this
+
+    if (dados.Type === 'series') {
+      this.duracao = `${dados.Runtime} por episódio, em média`
+    } else {
+      this.duracao = dados.Runtime || 'N/A';
+    }
+
+
+    if (this.direcao === 'N/A' && dados.Type === 'series') {
+      this.direcao = ` Temporadas: ${dados.totalSeasons}`;
+    }
+    else {
+      this.direcao = ` Diretor(a): ${dados.Director}`
+    }
+
   }
-
-
-  dataReq: Date = new Date();
 
 }
